081b247991eac987be36a7c5ad7366f4229a5f9d
MilosAksic <milosaksic@hotmail.com>
2019-09-19 18:20:48 +0200

    Header and footer changes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const header = (props) => {
+    return (
+        <div className="Header">
+            <p>I am a Milos and i am 21 years old</p>
+            
+        </div>
+    
+    )
+}
+
+export default header
---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css'
+
+const main = (props) => {
+    return (
+        <div className="Main">
+            <p>THIS IS Main</p>
+            
+        </div>
+    
+    )
+}
+
+export default main
