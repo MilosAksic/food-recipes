8102d29fa28c6b1bcb8504fce2a2f37edcb35b1c
MilosAksic <milosaksic@hotmail.com>
2019-09-21 23:32:36 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -8,17 +8,66 @@ import Footer from './components/Footer/Footer'
 import UpperHeader from './components/Header/UpperHeader/UpperHeader'
 import SingleCategory from './components/SingleCategory/SingleCategory'
 import SingleMeal from './components/SingleMeal/SingleMeal'
+import Category from './components/Main/Welcome/Category'
+
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 
 class App extends Component {
+
+  state = {
+    categories: [],
+    choosedCategory:''
+    
+  }
+  componentDidMount(){
+   axios.get('/categories.php').then (response => {
+      console.log(response.data.categories)
+      this.setState({
+        ...this.state,
+        categories: response.data.categories
+      })
+    })
+}
+
+clickHandler(CategoryName){
+  this.setState (
+    {
+      ...this.state,
+      choosedCategory:CategoryName
+    }
+  )
+}
+
+
   render(){
+    const categories = this.state.categories.map(
+      category => {
+        return <Link to="/categories" className='Category'
+                                      >
+          <Category
+         onClick={() => this.clickHandler(category.strCategory)}
+         categoryName = {category.strCategory} 
+         key={category.strCategory}
+         imgLink={category.strCategoryThumb}/>
+        </Link>
+         
+      }
+    )
     return (
       <div className="App">
         <BrowserRouter>
         <UpperHeader/>
 
-        <Route path="/" exact component={Homepage} />
-        <Route path="/categories" component={SingleCategory} />
+        <Route path="/" exact render={()=>{
+          return <Homepage categoriesList = {categories}/>
+        }} />
+
+        <Route path="/categories" render={()=>{
+          return <SingleCategory choosenCat = {this.state.choosedCategory}/>
+        }} />
+
         <Route path="/meal" component={SingleMeal} />
                  
         <Footer />
--- src/components/SingleCategory/SingleCategory.js
+++ src/components/SingleCategory/SingleCategory.js
@@ -1,7 +1,5 @@
-import React , { Component } from 'react';
+import React from 'react';
 import './SingleCategory.css';
-import Welcome from '../Main/Welcome/Welcome'
-
 
 import { Link } from 'react-router-dom'
 import axios from 'axios'
@@ -9,34 +7,42 @@ import axios from 'axios'
 import SingleCategoryMeal from './SingleCategoryMeal/SingeCategoryMeal'
 import FeaturedMeal from './FeaturedMeal/FeaturedMeal' 
 
+const FetchedCategory = 
+         axios.get('/filter.php?c=Beef').then (response => response.json())
+         .then( data => {
+             return data.data.meals
+         })
+console.log('fetched',FetchedCategory)
+//   const meals = FetchedCategory.map(
+//                 meal => {
+//                     return <Link to="/meal" className="SingleCategoryMeal" key={meal.idMeal}><SingleCategoryMeal          
+//                     mealName = {meal.strMeal}
+//                     link = {meal.strMealThumb}
+//                     key={meal.idMeal}/>
+//                     </Link>
+//                 }
+//             )
 
+const SingleCategory  =(props)=> {
+    // const FetchedCategory = 
+    //      axios.get('/filter.php?c=Beef').then (response => {
+    //         console.log(response.data.meals)
+    //          return response.data.meals
+    //            } )
+    // componentDidMount(){
 
-class SingleCategory extends Component {
-    state = {
-        meals: []
-      }
-    componentDidMount(){
+    //     // /filter.php?c=Seafood
+    //     axios.get('/filter.php?c=Beef').then (response => {
+    //         console.log(response.data.meals)
 
-        // /filter.php?c=Seafood
-        axios.get('/filter.php?c=Beef').then (response => {
-            console.log(response.data.meals)
-            console.log(Welcome.state)
-            this.setState({
-                meals: response.data.meals
-            })
-          })
+    //         this.setState({
+    //             meals: response.data.meals
+    //         })
+    //       })
 
-     }
-    render() {
-        const meals = this.state.meals.map (
-            meal => {
-                return <Link to="/meal" className="SingleCategoryMeal"><SingleCategoryMeal          
-                mealName = {meal.strMeal}
-                link = {meal.strMealThumb}
-                key={meal.idMeal}/>
-                </Link>
-            }
-        )
+    //  }
+       
+        
         return(
             <div className="SingleCategory">
                 <h2>KATEGORIJA </h2>
@@ -44,7 +50,7 @@ class SingleCategory extends Component {
                 <FeaturedMeal/>
                 <hr/>
                 <div className="allMeals">
-                      {meals}
+                      {/* {meals} */}
                 </div>
                 
             
@@ -54,6 +60,6 @@ class SingleCategory extends Component {
 
     }
         
-    }
+    
 
 export default SingleCategory
