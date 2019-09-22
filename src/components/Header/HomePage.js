50af98205b7b7b907302b356337fa02f5c7c2ed2
MilosAksic <milosaksic@hotmail.com>
2019-09-22 13:26:35 +0200

    Minor fixes

--- src/components/Header/HomePage.js
+++ src/components/Header/HomePage.js
@@ -16,7 +16,7 @@ const Homepage = (props) => {
                             <h3>Food recipes</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                             
-                            <button>Categories</button>
+                            <button>Categories </button>
                         </div>
 
                         <div className="right">
--- src/components/Main/Contact/Contact.js
+++ src/components/Main/Contact/Contact.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React , { Component } from 'react';
 import './Contact.css'
 
-const contact = (props) => {
-    const handleSubmit = event => {
+class contact extends Component {
+
+    state= {
+        firstName:'',
+        lastName:'',
+        Email:'',
+        Message:'',
+        isModalOpened:false
+    }
+    handleSubmit = event => {
        
         event.preventDefault();
-      };
+    };
+    
+    changedNameHandler(){
+              
+    }
+    changedLastNameHandler(){
+              
+    }
 
-    return (
-        <div className="Contact">
+    changedEmailHandler(){
+              
+    }
+    render(){
+        const handleSubmit = event => {
+       
+            event.preventDefault();
+          };
+          
+        return(
+            <div className="Contact">
             <h2>Contact</h2>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="First Name"></input>
+                <input type="text" placeholder="First Name"
+                onChange={this.changedName}></input>
                 <input type="text" placeholder="Last Name"></input>
                 <input type="email" placeholder="Email"></input>
                 <textarea placeholder="Message"/>
                 <button type="submit"
                 >Send</button>
             </form>
-        </div>
+        </div>    
+        )
+    }
+       
     
-    )
+    
+
+    
 }
 
 export default contact
