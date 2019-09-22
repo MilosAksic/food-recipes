4f46085cbd9100e14da6e2c737e97be9180900a0
MilosAksic <milosaksic@hotmail.com>
2019-09-22 20:53:41 +0200

    changes

--- src/components/SingleCategory/SingleCategory.js
+++ src/components/SingleCategory/SingleCategory.js
@@ -7,14 +7,15 @@ const SingleCategory  =(props)=> {
         return(
             <div className="SingleCategory">
 
-                    <h3>KATEGORIJA </h3>
+            
             <div className="FeaturedMeal">
-                    <h3>Our recomendation</h3>
-                    <img src={props.feateredURL}alt="slika"/>
-                    <p>{props.meals.strMeal} </p>
+                 <h3>{props.categoryName}</h3>
+                    <p className="recomendationText">Our recomendation</p>
+                    
+                    <div className="recomendation">{props.meals[0]}</div> 
             </div>
-                <h3>{props.categoryName}</h3>
-                {/* <FeaturedMeal meals={props.meals}/> */}
+                
+                
                 <hr/>
                 <div className="allMeals">
                        {props.meals} 
