a379bed3e2214150a0f9a42be5786c090d7dfbba
MilosAksic <milosaksic@hotmail.com>
2019-09-23 20:38:29 +0200

    Diffrent structure of app

--- src/components/SingleCategory/SingleCategory.js
+++ src/components/SingleCategory/SingleCategory.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './SingleCategory.css';
+import '../Header/SearchBar/SearchBar.css'
 
+import SearchIcon from '../../assets/Icons/search.png'
 
 const SingleCategory  =(props)=> {
-              
+    const handleSubmit = event => {     
+        event.preventDefault();
+      };    
         return(
             <div className="SingleCategory">
 
@@ -14,6 +18,21 @@ const SingleCategory  =(props)=> {
                     
                     <div className="recomendation">{props.meals[0]}</div> 
             </div>
+
+            <div className="Searchbar">
+                    <form onSubmit={handleSubmit}>
+                    <input type="text"
+                        placeholder="Search recipes"
+                        >
+
+                        </input>
+                    <button type="submit"
+                    
+                    >
+                        <img src={SearchIcon} alt='search'/>
+                    </button>
+                    </form>
+             </div>
                 
                 
                 <hr/>
